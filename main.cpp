b94c0ece09a67e4253c854defee6d1adf66c6365
Felino203 <felixnoel2001@gmail.com>
2021-07-24 17:24:09 -0400

    Compilation + debugging functionnal

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+//#include "model/minefield.hpp"
+
+using namespace std;
+
+void clearConsole()
+{
+    for (int i = 0; i < 20; i++)
+    {
+        cout << "\n";
+    }
+}
+
+int main()
+{
+    clearConsole();
+
+    cout << "console display: \n\n";
+}
